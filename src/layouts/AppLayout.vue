38f89c20befee4cb300cd74babed38a602f21c71
Gerardo Lucero <34350583+gerardolucero1@users.noreply.github.com>
2020-04-02 14:36:14 -0700

    se añadio un login basico

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <q-layout view="lHh Lpr lFf">
+        <q-page-container>
+            <router-view />
+        </q-page-container>
+    </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'AppLayout',
+
+  components: {
+
+  },
+
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
