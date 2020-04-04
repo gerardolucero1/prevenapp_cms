b3d68e857e7491d864ccc5660bb2013bcfdb0461
Eduardo <Eduardo@partnergrammer.com>
2020-04-03 21:15:39 -0700

    asd

--- src/pages/user/Login.vue
+++ src/pages/user/Login.vue
@@ -6,7 +6,7 @@
                     <p style="margin-top: 10px; font-size: 22px;">LOGIN</p>
                 </section>
 
-                <section class="row">
+                <section class="row" style="display:none">
                     <div class="col-12">
                         <q-input v-model="formulario.email" type="email" label="Email" />
                     </div>
@@ -19,12 +19,14 @@
                 </section>
 
                 <section class="row" style="display: flex; justify-content: center;">
-                    <p style="margin-top: 10px;">O ingresa con:</p>
+                    <p style="margin-top: 10px;"> ingresa con:</p>
                 </section>
 
                 <section class="row" style="display: flex; justify-content: center; flex-direction: column;">
-                    <q-btn style="margin-top: 10px;" color="primary" label="Facebook" @click="ingresar('facebook')" />
-                    <q-btn style="margin-top: 10px;" color="primary" label="Google" @click="ingresar('google')" />
+                    <q-btn style="margin-top: 10px; display:none" color="primary" label="Facebook" @click="ingresar('facebook')" />
+                    <q-btn style="margin-top: 10px;" color="primary" id="accesoGoogle" label="Google" @click="ingresar('google')" />
+
+                    <q-btn style="margin-top: 10px; display:none" color="green" id="btnAccesar" label="Acceder" @click="accesar()" />
                 </section>
             </div>
         </section>
@@ -83,6 +85,7 @@ export default {
         },
 
         async ingresarGoogle(){
+            
             let provider = new firebase.auth.GoogleAuthProvider()
             provider.setCustomParameters({
                 'display': 'popup'
@@ -92,10 +95,15 @@ export default {
                 let response = await auth.signInWithPopup(provider)
                 //console.log(response.user.uid)
                 this.getUser(response.user.uid)
+                document.getElementById('accesoGoogle').style.display="none";
+                document.getElementById('btnAccesar').style.display="block";
             }
             catch(error){
                 console.log(error)
             }
+        },
+        accesar(){
+             window.location.replace("https://prevenapp-cms.now.sh/#/form");
         },
 
         async ingresarEmail(){
